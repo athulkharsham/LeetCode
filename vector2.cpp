c65afd19f917daabfe8ea1be664cae97cb5ce102
athulkharsham <athulkharsham@gmail.com>
2024-12-05 17:47:52 +0530

    Added find maxmin function

--- vector2.cpp
+++ vector2.cpp
@@ -23,6 +23,33 @@ void removeElement(vector<int>& nums, int val)
     nums.resize(count);
 }
 
+/*
+Input:
+
+myList: [3, 7, 2, 9, 5]
+
+Output:
+
+[9, 2]
+
+*/
+
+vector<int> findMaxMin(vector<int>& myList)
+{
+    int max = myList[0];
+    int min = myList[0];
+
+    for(int i=0; i < myList.size(); i++)
+    {
+        if(myList[i] >= max)
+            max = myList[i];
+
+        if(myList[i] <= min)
+            min = myList[i];
+    }
+    return vector<int>{max, min};
+}
+
 
 // Display elements of input vector
 void display_vector(const vector<int> &v)
@@ -40,8 +67,11 @@ int main()
 
     // display_vector(vect);
 
-    removeElement(vect, 3);
-    display_vector(vect);
+    // removeElement(vect, 3);
+
+    vector<int> maxMin{3, 7, 2, 9, 5};
+    auto result = findMaxMin(maxMin);
+    display_vector(result);
 
     return 0;
 }
